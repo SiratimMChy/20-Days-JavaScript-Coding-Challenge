51deb2f6ff61071d0451fa2737f425b060aa0768
Tuha chy <siratimmustakimchowdhury123@gmail.com>
2026-06-23 23:59:03 +0600

    Day 7

---
+++ Day7/P31.js
@@ -0,0 +1,31 @@
+/*Problem 31: Debounce Function  [Medium]
+Description: Write a debounce(fn, delay) function that delays invoking fn until after delay milliseconds have elapsed since the last time it was called.
+Example:
+const debouncedSearch = debounce(search, 300);// Fires only once after user stops typing for 300ms
+Hint: Use clearTimeout and setTimeout.
+*/
+function debounce(fn, delay) {
+  let time;
+
+  return function (...args) {
+    clearTimeout(time);
+
+    time = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
+function search(query) {
+  console.log("Searching for:", query);
+}
+
+const debouncedSearch = debounce(search, 300);
+
+debouncedSearch("S");
+debouncedSearch("Sir");
+debouncedSearch("Sirati");
+debouncedSearch("SiratimMus");
+debouncedSearch("SiratimMusta");
+debouncedSearch("Siratim Mustakim");
+
